992628f851d94be43a51f3bafa443c509eff0d4f
Zakiy Luthfi <zakiymluthfi@gmail.com>
2024-09-20 13:14:03 +0700

    Materi & praktek Hari ke-12 <Hari Masuk (M) ke-9

---
+++ 09-EJS.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+const fs = require("fs");
+
+app.set("view engine", "ejs");
+
+// Rute khusus untuk file HTML
+app.get("/", (req, res) => {
+  const nama = "Zakiy M Luthfi";
+  res.render("index", { name: nama, title: "Homepage" });
+  // res.sendFile((__dirname, "Hari-ke-11", "html", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { title: "This is about" });
+
+  // res.sendFile((__dirname, "Hari-ke-11", "html", "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  fs.readFile("./views/data/contacs.json", "utf-8", (err, file) => {
+    let contacts = [];
+    let errorMessage = "File tidak ada";
+    if (!err) {
+      try {
+        contacts = JSON.parse(file);
+        errorMessage = "";
+      } catch (parseError) {
+        errorMessage = "Terdapat kesalahan saat parsing data";
+      }
+    }
+
+    res.render("contact", {
+      contacts,
+      errorMessage,
+      title: "This is contact",
+    });
+  });
+});
+
+app.use("/product/:prodID", (req, res) => {
+  res.send(
+    `product ID: ${req.params.prodID} <br>category ID: ${req.params.catID}`
+  );
+});
+
+// Penanganan rute yang tidak ditemukan
+app.use((req, res) => {
+  res.status(404).send("Page not found: 404");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
